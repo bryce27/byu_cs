3d69a95a6d4ba4439019971d79c23964d25dc445
Bryce <byoung888@gmail.com>
2015-03-23 12:41:40 -0600

    Added screenshots of grades and project descriptions

---
+++ cs235/Lab 9 - AVL Tree/Factory.cpp
@@ -0,0 +1,20 @@
+#include "Factory.h"
+#include "AVL.h"
+//You may add #include statements here
+
+/*
+	You will MODIFY THIS DOCUMENT.
+*/
+//=======================================================================================
+/*
+	getAVL()
+
+	Creates and returns an object whose class extends AVLInterface.
+	This should be an object of a class you have created.
+
+	Example: If you made a class called "AVL", you might say, "return new AVL();".
+*/
+AVLInterface * Factory::getAVL()
+{
+	return new AVL();//Modify this line
+}
